122c0ab6360fb4aac66d4eae5d316fab4eb76ad4
sarah-ross <sarahross025@gmail.com>
2022-03-28 19:36:41 -0400

    Added media queries

--- src/App.js
+++ src/App.js
@@ -6,28 +6,30 @@ import "./App.css";
 export default function App() {
 	return (
 		<div className="App">
-			<div className="card mt-5">
-				<h1>
-					<span>React</span> Weather App
-				</h1>
-				<WeatherForm />
-				<br />
+			<div className="container">
+				<div className="card mt-5">
+					<h1>
+						<span>React</span> Weather App
+					</h1>
+					<WeatherForm />
+					<br />
+				</div>
+				<footer className="m-2">
+					Open-source code by
+					<a
+						href="https://www.sarah-codes.com/"
+						className="portfolioLink">
+						{" "}
+						Sarah Ross
+					</a>{" "}
+					available on{" "}
+					<a
+						href="https://github.com/sarah-ross/react-weather-app"
+						className="githubLink">
+						GitHub 👩‍💻
+					</a>
+				</footer>
 			</div>
-			<footer className="m-2">
-				Open-source code by
-				<a
-					href="https://www.sarah-codes.com/"
-					className="portfolioLink">
-					{" "}
-					Sarah Ross
-				</a>{" "}
-				available on{" "}
-				<a
-					href="https://github.com/sarah-ross/react-weather-app"
-					className="githubLink">
-					GitHub 👩‍💻
-				</a>
-			</footer>
 		</div>
 	);
 }
--- src/WeatherForecastDay.js
+++ src/WeatherForecastDay.js
@@ -1,6 +1,8 @@
 import React from "react";
 import WeatherIcon from "./WeatherIcon";
 
+import "./WeatherForecastDay.css";
+
 export default function WeatherForecastDay(props) {
 	function maxTemperature() {
 		let temperature = Math.round(props.data.temp.max);
